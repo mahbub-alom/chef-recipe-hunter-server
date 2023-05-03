d51def33bc77b98b1ccef6ee26f31cb01d8174ff
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-05-03 14:04:25 +0600

    recifes data created done

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 5000;
+const cors = require('cors')
+const port = process.env.PORT || 5000;
 
+const chefData = require('./data/chefdata.json')
+// const chefSingleData = require('./data/chefSingleData.json')
+
+app.use(cors())
 
 app.get(('/'), (req, res) => {
     res.send('hello world')
 })
+
+app.get('/chefData', (req, res) => {
+    res.send(chefData)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
